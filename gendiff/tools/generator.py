d8b6753d3f0ff03c2fdcfa9fa175c55e0b5ce9ae
Alexander <alexandrageev87@gmail.com>
2023-08-27 20:55:44 +0300

    small fix

--- gendiff/tools/generator.py
+++ gendiff/tools/generator.py
@@ -17,5 +17,5 @@ FORMATTERS = {
 def generate_diff(old_file, new_file, format=DEFAULT_FORMATTER):
     old_data = parse_data(*get_data(old_file))
     new_data = parse_data(*get_data(new_file))
-    data = compare_data(old_data, new_data, [])
+    data = compare_data(old_data, new_data, 0)
     return FORMATTERS.get(format, DEFAULT_FORMATTER)(data)
--- gendiff/tools/internal_representation.py
+++ gendiff/tools/internal_representation.py
@@ -1,87 +1,42 @@
 """Модуль предназначен для анализа изменений в двух наборах данных"""
 
 
-from gendiff.tools import (
-    ADDED, EQUAL, PASS, REMOVED,
-    UPDATED, NODE, TYPE_NODE, TYPE_LEAF
-)
+from gendiff.tools import (ADDED, EQUAL, REMOVED, UPDATED, NESTED)
 
 
-def get_children(data, path):
-    """Функция возвращает список всех вложенных параметров
-    сложносоставного параметра"""
-    childrens = []
-    if not isinstance(data, dict):
-        return childrens
-    keys = sorted(data.items(), reverse=True)
-    for key, value in keys:
-        childrens.append(get_compare_record(path, key, value))
-    return childrens
-
-
-def get_compare_record(path, key, old_value, new_value=None, status=PASS):
-    """Функция записывает результат сравнения двух параметров в виде словаря"""
-    if isinstance(old_value, dict):
-        old_value_type = TYPE_NODE
-    else:
-        old_value_type = TYPE_LEAF
-    if isinstance(new_value, dict):
-        new_value_type = TYPE_NODE
-    else:
-        new_value_type = TYPE_LEAF
-    res = {
-        'path': path,
-        'key': key,
-        'old_value': old_value,
-        'old_value_type': old_value_type,
-        'new_value': new_value,
-        'new_value_type': new_value_type,
-        'status': status
-    }
-    return res
-
-
-def compare_data(old_data, new_data, path):  # noqa: C901
+def compare_data(old_data, new_data, level):  # noqa: C901
     """
     Функция получает на вход два словаря с наборами параметров
     и путь к этим параметрам. Сверяет каждый параметр в этих словарях.
     Результат выводится в виде списка данных о статусах каждого параметра.
-    ADDED - параметр добавлен в новый файл
-    REMOVED - параметр удален из нового файла
-    EQUAL - значения параметров одинаковые в обоих файлах
-    NODE - параметр является вложенной структурой
-    UPDATED - значение параметра изменено
-    PASS - сравнение не выполнялось
+    Статусы описаны в файле gendiff/tools/__init__.py
     """
     sorted_keys = sorted(old_data.keys() | new_data.keys())
     diff = []
     for key in sorted_keys:
         if key not in old_data:
             status = ADDED
-            old_value = None
-            new_value = new_data[key]
+            value = new_data[key]
         elif key not in new_data:
             status = REMOVED
-            old_value = old_data[key]
-            new_value = None
+            value = old_data[key]
         elif isinstance(old_data[key], dict) and isinstance(
                 new_data[key], dict):
-            status = NODE
-            new_value = new_data[key]
-            old_value = old_data[key]
+            status = NESTED
+            value = compare_data(old_data[key], new_data[key], level + 1)
         elif old_data[key] == new_data[key]:
             status = EQUAL
-            new_value = new_data[key]
-            old_value = old_data[key]
+            value = new_data[key]
         elif old_data[key] != new_data[key]:
             status = UPDATED
-            new_value = new_data[key]
-            old_value = old_data[key]
+            value = (old_data[key], new_data[key])
         else:
             assert True, 'Что-то пошло не по плану :('
-        rec = get_compare_record(path, key, old_value, new_value, status)
-        diff.append(rec)
-        if status == NODE:
-            new_path = path + [key]
-            diff.extend(compare_data(old_value, new_value, new_path))
+        data = {
+            "key": key,
+            "status": status,
+            "level": level,
+            "value": value
+        }
+        diff.append(data)
     return diff
